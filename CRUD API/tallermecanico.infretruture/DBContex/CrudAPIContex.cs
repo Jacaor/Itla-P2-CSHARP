9dc8e733c7cfd5ddaca2e8ad74fa23adaa20bcc9
Jacaor <jambrielcalderonortiz@gmail.com>
2026-03-21 16:24:38 -0300

    P2 API 1

---
+++ CRUD API/tallermecanico.infretruture/DBContex/CrudAPIContex.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using tallermecanico.infretruture.Model;
+namespace tallermecanico.infretruture.DBContex
+{
+    public class CrudAPIContex: DbContext
+    {
+        public CrudAPIContex(DbContextOptions<CrudAPIContex> options) : base(options)
+        {
+        }
+        public DbSet<CustomerModel> Customers { get; set; }
+    
+
+
+
+
+
+    }
+}
---
+++ CRUD API/tallermecanico.infretruture/Model/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+namespace tallermecanico.infretruture.Model
+{
+
+    public class CustomerModel
+
+    {
+        [Column("CustomerId")]
+        public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; } 
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; }
+
+
+
+
+    public CustomerModel(int customerid, string firstName, string phoneNumber, string email, string lastName)
+    {
+            Id = customerid;
+            FirstName = firstName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            LastName = lastName;
+    }
+
+    public CustomerModel() { }
+}
+
+}
